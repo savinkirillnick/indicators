cc27ee48b783aa00bf3b7df82923e494c39caf82
savinkirillnick <34914281+savinkirillnick@users.noreply.github.com>
2019-09-06 07:50:46 +0700

    Create ema.php

---
+++ ema.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+* Exponential Moving Average
+* array ema ( array $closes , integer $n  )
+* 
+* $closes is data array and $n is number of period
+* example:
+* $closes[0]    => earliest value
+* $closes[n]    => latest value
+* 
+*/
+
+function ema($closes, $n)
+{
+    $m   = count($closes);
+    $a   = 2 / ($n + 1);
+    $EMA = [];
+    $EMA[] = $closes[0];
+    for ($i = 1; $i < $m; $i++) {
+        $EMA[] = ($a * $closes[$i]) + ((1 - $a) * $EMA[$i - 1]);
+    }
+    return $EMA;
+}
+?>
